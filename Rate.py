0d6d230c82c9a0d084390ee0d3c482d41a9bbe65
Jimmie <116106035+Mataila@users.noreply.github.com>
2025-02-03 21:36:08 +0300

    Add files via upload

---
+++ Rate.py
@@ -0,0 +1,4 @@
+hours = float(input("Enter the number of hours worked: "))
+rate_per_hour = float(input("Enter the rate per hour: "))
+gross_pay = hours * rate_per_hour
+print(f"Gross pay: ${gross_pay:.2f}")
